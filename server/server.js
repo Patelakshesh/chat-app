e52bb55e30a0c1e821ba67ed451bfa29fa1bd02a
Akshesh <aksheshpatel2019@gmail.com>
2025-10-04 12:16:47 +0530

    chat app are done

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/user.routes.js';
-import messageRouter from './routes/message.routes.js';
-import { Server } from 'socket.io';
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/user.routes.js";
+import messageRouter from "./routes/message.routes.js";
+import { Server } from "socket.io";
 
 //create express app
 const app = express();
-const server= http.createServer(app);
+const server = http.createServer(app);
 
 // initialize socket.io
 export const io = new Server(server, {
-  cors: {origin: "*"}
-})
+  cors: { origin: "*" },
+});
 
 // store online users
 export const userSockerMap = {}; // {userId: socketId}
@@ -24,7 +24,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   console.log("User connected with ID:", userId);
 
-  if(userId) userSockerMap[userId] = socket.id;
+  if (userId) userSockerMap[userId] = socket.id;
 
   //Emit online users to all connected users
   io.emit("getOnlineUsers", Object.keys(userSockerMap));
@@ -33,29 +33,25 @@ io.on("connection", (socket) => {
     console.log("User disconnected with ID:", userId);
     delete userSockerMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSockerMap));
-  })
-})
+  });
+});
 
 //middlewares
 app.use(cors());
-app.use(express.json({limit: '4mb'}));
+app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/status", (req, res) => res.send("API is running..."));
 app.use("/api/auth", userRouter);
-app.use("/api/messages", messageRouter)
+app.use("/api/messages", messageRouter);
 
 //connect to DB
-await connectDB()
-
-if(process.env.NODE_ENV === "production"){
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-}
 
 // export server for vercel
 export default server;
